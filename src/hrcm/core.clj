88acaecd0eb2699cebbcad248ee2568b1ca5c1d7
liquidz <liquidz.uo@gmail.com>
2017-08-23 15:10:34 +0000

    dev

--- src/hrcm/core.clj
+++ src/hrcm/core.clj
@@ -1,79 +1,138 @@
 (ns hrcm.core)
 
-(def ^:private inboxes  (atom []))
-(def ^:private outboxes (atom []))
-;(def ^:private register (atom {}))
-;(def ^:private holding  (atom {}))
+(defn- inc-pos
+  [c]
+  (update c :pos inc))
 
 (defn inbox
-  []
-  (let [hold (first @inboxes)]
-    (swap! inboxes rest)
-    hold))
+  [{[x & rst] :inbox :as c}]
+  (if x
+    (assoc c :inbox rst :holding x)
+    (assoc c :finish true)))
 
 (defn outbox
-  [holding]
-  (if-not holding
-    (throw (ex-info "not holding" {})))
-  (swap! outboxes conj holding))
+  [{:keys [holding outbox] :or {outbox []} :as c}]
+  (if holding
+    (assoc c :holding nil :outbox (conj outbox holding))
+    (assoc c :failed "FIXME")))
+
+(defn- address
+  [register target]
+  (if (sequential? target)
+    (get register (first target) nil)
+    target))
 
 (defn copyto
-  [hold target]
-  )
+  [{:keys [register holding] :or {register {}} :as c} target]
+  (if-not holding
+    (assoc c :failed "FIXME")
+    (update c :register
+            assoc (address register target) holding)))
 
-(defn run*
-  [programs]
-  (let [pos' (atom 0)]
-    (try
-      (loop [pos 0
-             holding nil]
-        (reset! pos' pos)
-        (let [[op & args] (nth programs pos nil)]
-          (case op
-            :inbox
-            (if-let [hold (inbox)]
-              (recur (inc pos) hold))
+(defn copyfrom
+  [{:keys [register] :or {register {}} :as c} target]
+  (if-let [x (get register (address register target))]
+    (assoc c :holding x)
+    (assoc c :failed "no data")))
 
-            :outbox
-            (do (outbox holding)
-                (recur (inc pos) nil))
+(defn label
+  [c id]
+  c)
 
-            ;:copyto
-            ;(do
-            ;  (apply copyto holding args)
-            ;  )
+(defn- find-label-index
+  [codes id]
+  (reduce
+    (fn [index code]
+      (if (= code [:label id])
+        (reduced index)
+        (inc index)))
+    0 codes))
 
-            "finish")))
-        (catch Exception e
-          )
-        )
-    )
-      (catch Exception e
-        (throw (ex-info
-                 (.getMessage e)
-                 (merge (ex-data e) {:position pos :holding holding})))
-        nil))
-    )
-  )
+(defn jump
+  [{:keys [codes] :as c} id]
+  (assoc c :pos (find-label-index codes id)))
 
-(defn run
-  [& args]
-  (try
-    (apply run* args)
-    (catch Exception e
-      (println e))))
+(defn jump-if-zero
+  [{:keys [holding codes] :as c} id]
+  (cond
+    (not holding)
+    (assoc c :failed "not holding")
 
+    (not (number? holding))
+    (assoc c :failed "must be number")
 
-(comment
-  (do
-    (reset! inboxes [1 2])
-    (reset! outboxes [])
-    (run '[
-           ;(:inbox)
-           ;(:copyto 1)
-           ;(:copyto [1])
-           (:outbox)
-           ])
-    (println @inboxes @outboxes)
-    )
-  )
+    (zero? holding)
+    (assoc c :pos (find-label-index codes id))
+
+    :else c))
+
+(defn jump-if-neg
+  [{:keys [holding codes] :as c} id]
+  (cond
+    (not holding)
+    (assoc c :failed "not holding")
+
+    (not (number? holding))
+    (assoc c :failed "must be number")
+
+    (< holding 0)
+    (assoc c :pos (find-label-index codes id))
+
+    :else c))
+
+;(defn run*
+;  [programs]
+;  (let [pos' (atom 0)]
+;    (try
+;      (loop [pos 0
+;             holding nil]
+;        (reset! pos' pos)
+;        (let [[op & args] (nth programs pos nil)]
+;          (case op
+;            :inbox
+;            (if-let [hold (inbox)]
+;              (recur (inc pos) hold))
+;
+;            :outbox
+;            (do (outbox holding)
+;                (recur (inc pos) nil))
+;
+;            ;:copyto
+;            ;(do
+;            ;  (apply copyto holding args)
+;            ;  )
+;
+;            "finish")))
+;        (catch Exception e
+;          )
+;        )
+;    )
+;      (catch Exception e
+;        (throw (ex-info
+;                 (.getMessage e)
+;                 (merge (ex-data e) {:position pos :holding holding})))
+;        nil))
+;    )
+;  )
+;
+;(defn run
+;  [& args]
+;  (try
+;    (apply run* args)
+;    (catch Exception e
+;      (println e))))
+;
+;
+;(comment
+;  (do
+;    (reset! inboxes [1 2])
+;    (reset! outboxes [])
+;    (run '[
+;           ;(:inbox)
+;           ;(:copyto 1)
+;           ;(:copyto [1])
+;           (:outbox)
+;           ])
+;    (println @inboxes @outboxes)
+;    )
+;  )
--- test/hrcm/core_test.clj
+++ test/hrcm/core_test.clj
@@ -1,7 +1,79 @@
 (ns hrcm.core-test
-  (:require [clojure.test :refer :all]
-            [hrcm.core :refer :all]))
+  (:require
+    [clojure.test :refer :all]
+    [hrcm.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest inbox-test
+  (are [before after] (= (inbox before) after)
+    {:inbox [1 2] :holding nil}
+    {:inbox [2] :holding 1}
+    
+    {:inbox [] :holding nil}
+    {:inbox [] :holding nil :finish true}))
+
+(deftest outbox-test
+  (are [before after] (= (outbox before) after)
+    {:holding 1 :outbox [0]}
+    {:holding nil :outbox [0 1]}
+    
+    {:holding 1}
+    {:holding nil :outbox [1]})
+  (is (contains? (outbox {}) :failed)))
+
+(deftest copyto-test
+  (are [before target after] (= (copyto before target) after)
+    {:holding 1}
+    :target
+    {:holding 1 :register {:target 1}}
+
+    {:holding 1 :register {:addr 2}}
+    [:addr]
+    {:holding 1 :register {:addr 2, 2 1}})
+  (is (contains? (copyto {} nil) :failed)))
+
+(deftest copyfrom-test
+  (are [before target after] (= (copyfrom before target) after)
+    {:register {:target 1}}
+    :target
+    {:holding 1 :register {:target 1}}
+
+    {:register {:addr 2, 2 1}}
+    [:addr]
+    {:holding 1 :register {:addr 2, 2 1}})
+  (is (contains? (copyfrom {} :x) :failed)))
+
+(deftest jump-test
+  (are [before id after] (= (jump before id) after)
+    {:codes [0 [:label "foo"] 2] :pos 99}
+    "foo"
+    {:codes [0 [:label "foo"] 2] :pos 1}))
+
+(deftest jump-if-zero-test
+  (are [before id after] (= (jump-if-zero before id) after)
+    {:holding 0 :codes [0 [:label "foo"] 2] :pos 99}
+    "foo"
+    {:holding 0 :codes [0 [:label "foo"] 2] :pos 1}
+    
+    {:holding 1 :codes [0 [:label "foo"] 2] :pos 99}
+    "foo"
+    {:holding 1 :codes [0 [:label "foo"] 2] :pos 99})
+
+  (doseq [c [{} {:holding "A"}]]
+    (is (contains? (jump-if-zero c nil) :failed))))
+
+(deftest jump-if-neg-test
+  (are [before id after] (= (jump-if-neg before id) after)
+    {:holding -1 :codes [0 [:label "foo"] 2] :pos 99}
+    "foo"
+    {:holding -1 :codes [0 [:label "foo"] 2] :pos 1}
+    
+    {:holding 0 :codes [0 [:label "foo"] 2] :pos 99}
+    "foo"
+    {:holding 0 :codes [0 [:label "foo"] 2] :pos 99})
+  
+  (doseq [c [{} {:holding "A"}]]
+    (is (contains? (jump-if-neg c nil) :failed))))
+
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
